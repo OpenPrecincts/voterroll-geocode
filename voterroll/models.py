51003c4b7dfc4795f625ea0d45bcb97896da186d
James Turk <dev@jamesturk.net>
2019-04-23 15:24:19 -0400

    MultiPoly, and update Pipfile

--- voterroll/models.py
+++ voterroll/models.py
@@ -27,11 +27,11 @@ class VoterRecord(models.Model):
     latest_geocode_result = models.CharField(
         max_length=1,
         choices=(
-            ('G', 'Geocoded'),
-            ('X', 'No Result From Geocoder'),
-            (' ', 'No Attempt'),
+            ("G", "Geocoded"),
+            ("X", "No Result From Geocoder"),
+            (" ", "No Attempt"),
         ),
-        default=' '
+        default=" ",
     )
     geocoded_address = models.CharField(max_length=300, blank=True)
     geocode_is_exact = models.BooleanField(default=False)
@@ -42,20 +42,15 @@ class VoterRecord(models.Model):
     tract = models.CharField(max_length=8, blank=True)
     block = models.CharField(max_length=8, blank=True)
 
-
     class Meta:
-        indexes = [
-            models.Index(fields=['state', 'latest_geocode_result']),
-        ]
+        indexes = [models.Index(fields=["state", "latest_geocode_result"])]
 
 
 class County(models.Model):
     state_fips = models.CharField(max_length=2)
     county_fips = models.CharField(max_length=4)
     name = models.CharField(max_length=100)
-    poly = models.PolygonField()
+    poly = models.MultiPolygonField()
 
     def __str__(self):
         return self.name
-
-
